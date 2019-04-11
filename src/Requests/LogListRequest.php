bf81249647c7692c0bbd674d4887becf8ae86558
Volga <vtikunov@yandex.ru>
2019-04-11 22:07:32 +0300

    Стилистические правки

--- src/Requests/LogListRequest.php
+++ src/Requests/LogListRequest.php
@@ -7,6 +7,11 @@ namespace Volga\MetrikaLogs\Requests;
 use Volga\MetrikaLogs\Contracts\Request;
 use Volga\MetrikaLogs\Requests\Concerns\RequestCore;
 
+/**
+ * Class LogListRequest
+ *
+ * @package Volga\MetrikaLogs\Requests
+ */
 class LogListRequest extends RequestCore implements Request
 {
     protected const METHOD = 'GET';
